139657387ccf0735e1f43eedcd5097645e5c6d99
imnotrealokay <105939539+imnotrealokay@users.noreply.github.com>
2022-07-22 23:59:21 -0700

    Add files via upload

---
+++ weather/code.js
@@ -0,0 +1,22 @@
+
+function procces_(data){
+    jQuery('#city_temperature').html(data.main.temp)
+  }
+  
+function GET_AJAX(){
+  city_name_input = jQuery('#city_name_input').val()
+  $.ajax(
+    {
+        url:`https://api.openweathermap.org/data/2.5/weather?q=${city_name_input}&appid=b660f3402c54cb9a9c48f89c35249e5c&unit=metric`,
+        type:"GET",
+        success: procces_
+      }
+
+    )
+    }
+  
+function setup(){
+  $('#get_temperature_button').click(GET_AJAX);
+}
+$(document).ready(setup);
+  
